11dc3de4622245231fcecaf3e2b86e8a9f96b352
Milos <miloshhm@gmail.com>
2024-05-21 02:11:25 +0200

    replace md5 with bcrypt

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -46,22 +45,25 @@ app.get("/register", function (req, res) {
 });
 
 app.post("/register", async function (req, res) {
-    try {
-        // Create a new user document
-        const newUser = new User({
-            email: req.body.username,
-            password: md5(req.body.password),
-        });
+    bcrypt.hash(req.body.password, saltRounds, async function (err, hash) {
+        // Store hash in your password DB.
+        try {
+            // Create a new user document
+            const newUser = new User({
+                email: req.body.username,
+                password: hash,
+            });
 
-        // Save the user document (using async/await)
-        await newUser.save();
-        console.log("User registered successfully!");
-        res.render("secrets");
-    } catch (error) {
-        console.error("Error registering user:", error);
-        // Handle the error (e.g., display an error page)
-        res.status(500).send("Error registering user. Please try again later.");
-    }
+            // Save the user document (using async/await)
+            await newUser.save();
+            console.log("User registered successfully!");
+            res.render("secrets");
+        } catch (error) {
+            console.error("Error registering user:", error);
+            // Handle the error (e.g., display an error page)
+            res.status(500).send("Error registering user. Please try again later.");
+        }
+    });
 });
 
 //Using Promises
@@ -88,11 +90,13 @@ app.post("/register", async function (req, res) {
 //async/await
 app.post("/login", async function (req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     try {
         const foundUser = await User.findOne({ email: username });
-        if (foundUser && foundUser.password === password) {
+        const passwordMatch = await bcrypt.compare(password, foundUser.password);
+
+        if (passwordMatch) {
             res.render("secrets");
         } else {
             // Handle invalid login (e.g., show an error message)
