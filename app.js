8fc4e350977109770453857ce4188a8a71683b08
miloshhm <44277385+miloshhm@users.noreply.github.com>
2024-05-24 04:33:27 +0200

    Update app.js
    remove unnesesery console

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ passport.use(
         },
 
         function (accessToken, refreshToken, profile, cb) {
-            console.log(profile);
+           
             User.findOrCreate({ googleId: profile.id }, function (err, user) {
                 return cb(err, user);
             });
